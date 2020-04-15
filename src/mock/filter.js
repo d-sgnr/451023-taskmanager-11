520c9fc75b6d92835eae30ff636a1ff2f96f0673
David <dm.dsgnr@gmail.com>
2020-04-15 19:13:53 +0300

    Вводит компонентный подход

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -2,15 +2,11 @@ const filterNames = [
   `all`, `overdue`, `today`, `favorites`, `repeating`, `archive`
 ];
 
-const generateFilters = () => {
+export const generateFilters = () => {
   return filterNames.map((it) => {
     return {
       name: it,
       count: Math.floor(Math.random() * 10),
     };
   });
-};
-
-export {
-  generateFilters
 };
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,32 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 const castTimeFormat = (value) => {
   return value < 10 ? `0${value}` : String(value);
 };
 
-const formatTime = (date) => {
+export const formatTime = (date) => {
   const hours = castTimeFormat(date.getHours() % 12);
   const minutes = castTimeFormat(date.getMinutes());
-
   return `${hours}:${minutes}`;
 };
 
-export {formatTime};
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
