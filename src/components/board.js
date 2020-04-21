d1ad0e83c5fcc81dc78daf5a75992aba2b6aaa07
David <dm.dsgnr@gmail.com>
2020-04-21 20:06:29 +0300

    Рефакторит код и исправляет ошибки

--- src/components/board.js
+++ src/components/board.js
@@ -10,4 +10,4 @@ export default class Board extends AbstractComponent {
   getTemplate() {
     return createBoardTemplate();
   }
-};
+}
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,11 +1,11 @@
-import {ESC_KEYCODE, ENTER_KEYCODE} from '../const.js'
+import {ESC_KEYCODE, ENTER_KEYCODE} from '../const.js';
 
 export const isEscKeyDown = (evt, action) => {
-  evt.keyCode === ESC_KEYCODE ? action() : ``;
+  return evt.keyCode === ESC_KEYCODE ? action() : ``;
 };
 
 export const isEnterKeyDown = (evt, action) => {
-  evt.keyCode === ENTER_KEYCODE ? action() : ``;
+  return evt.keyCode === ENTER_KEYCODE ? action() : ``;
 };
 
 const castTimeFormat = (value) => {
@@ -16,4 +16,25 @@ export const formatTime = (date) => {
   const hours = castTimeFormat(date.getHours() % 12);
   const minutes = castTimeFormat(date.getMinutes());
   return `${hours}:${minutes}`;
+};
+
+export const sortArray = (property, order = `-1`) => {
+  const sortOrder = order;
+
+  if (property[0] === `-`) {
+    sortOrder = 1;
+    property = property.substr(1);
+  }
+
+  return function (a, b) {
+    let result = 0;
+
+    if (a[property] < b[property]) {
+      result = -1;
+    } else if (a[property] > b[property]) {
+      result = 1;
+    }
+
+    return result * sortOrder;
+  };
 };
